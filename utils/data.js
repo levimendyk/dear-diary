71a3e54e6978a641391e091b10ac8367b8c0701d
Levi Mendyk <levimendyk@Levis-MacBook-Air.local>
2022-10-19 18:38:11 -0600

    added some more files and more base starting code

---
+++ utils/data.js
@@ -0,0 +1,18 @@
+// LEVI-STARTING DATA CODE
+const userinfo = [{
+    username;
+    emailer;
+    thoughts;
+    friends;
+}];
+
+const thoughts = [{
+    thoughtText;
+    createAtt;
+    username;
+    reactions;
+}];
+
+const users = [];
+
+module.exports = {} // LEVI-Not sure yet
---
+++ utils/seeds.js
@@ -0,0 +1,4 @@
+// LEVI-STARTING SEEDS CODE
+const connection = require('../config/connection');
+const { User, Application } = require('../models');
+
